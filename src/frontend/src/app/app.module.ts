ed611e783b39346798b7744a7ae1e87266b8028a
filber-9 <filber-9@student.ltu.se>
2023-09-19 13:28:25 +0200

    hmi: Add structure for google login api

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,16 +1,53 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {MainPageComponent} from './home/home-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {MatCardModule} from '@angular/material/card';
+//import {MatFormFieldModule} from '@angular/material/form-field';
+//import {MatButtonModule} from '@angular/material/button';
+//import {MatInputModule} from '@angular/material/input';
+import {GoogleLoginProvider, SocialLoginModule} from 'angularx-social-login';
+import {AuthGuardService} from './auth-guard.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'mainpage', component: MainPageComponent, canActivate: [AuthGuardService]},
+      {path: '**', component: LoginComponent}
+    ]),
+    BrowserAnimationsModule,
+    //MatCardModule,
+    //MatFormFieldModule,
+    //MatButtonModule,
+    //MatInputModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('148517665605-jspahbqleats6lvlag9kasc2c11b5g7o.apps.googleusercontent.com')
+        }
+      ]
+    }
+  },
+    AuthGuardService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
