504c6a09bc617263e3be37dd63e0e22368ce2c2c
TovahParnes <tovah@parnes.com>
2023-10-10 16:31:16 +0200

    added post endpoints, handler and controller functions, no functionality implemented

--- src/internal/handlers/routes_handler.go
+++ src/internal/handlers/routes_handler.go
@@ -2,11 +2,12 @@ package handlers
 
 import (
 	"birdai/src/internal/repositories"
+	"os"
+
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"os"
 )
 
 func New(app *fiber.App, db repositories.IMongoInstance) {
@@ -31,4 +32,12 @@ func New(app *fiber.App, db repositories.IMongoInstance) {
 	usersRoute.Post("/", handler.Login)
 	usersRoute.Patch("/:id", handler.UpdateUser)
 	usersRoute.Delete("/:id", handler.DeleteUser)
+
+	postsRoute := app.Group("/posts")
+	postsRoute.Get("/list", handler.ListPosts)
+	usersRoute.Get("/:id/posts/list", handler.ListUsersPosts)
+	postsRoute.Get("/:id", handler.GetPostById)
+	postsRoute.Post("/", handler.CreatePost)
+	postsRoute.Patch("/:id", handler.UpdatePost)
+	postsRoute.Delete("/:id", handler.DeletePost)
 }
--- src/setup.go
+++ src/setup.go
@@ -18,7 +18,7 @@ func Setup(ctx context.Context) (*fiber.App, error) {
 	handlers.New(app, db)
 
 	//these two lines needs to be there for swagger to fuction
-	docs.SwaggerInfo.Host = "localhost:" + os.Getenv("PORT")
+	docs.SwaggerInfo.Host = "127.0.0.1:" + os.Getenv("PORT")
 	app.Get("/swagger/*", swagger.HandlerDefault) // default
 
 	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
