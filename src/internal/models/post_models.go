c19b7a5520bcec646b3258292728597f8fbfac71
lindstromemil <emil.lindstrom00@outlook.com>
2023-11-13 15:56:24 +0100

    updated post models with comment and accuracy

--- src/internal/models/post_models.go
+++ src/internal/models/post_models.go
@@ -1,18 +1,22 @@
 package models
 
 type PostDB struct {
-	Id        string `bson:"_id"`
-	UserId    string `bson:"user_id"`
-	BirdId    string `bson:"bird_id"`
-	CreatedAt string `bson:"created_at"`
-	Location  string `bson:"location"`
-	MediaId   string `bson:"media_id"`
+	Id        string  `bson:"_id"`
+	UserId    string  `bson:"user_id"`
+	BirdId    string  `bson:"bird_id"`
+	CreatedAt string  `bson:"created_at"`
+	Location  string  `bson:"location"`
+	Comment   string  `bson:"comment"`
+	Accuracy  float32 `bson:"accuracy"`
+	MediaId   string  `bson:"media_id"`
 }
 
 type PostInput struct {
 	Id       string     `bson:"_id" json:"_id" form:"_id"`
 	BirdId   string     `bson:"bird_id" json:"birdId" form:"birdId"`
 	Location string     `bson:"location" json:"location" form:"location"`
+	Comment  string     `bson:"comment" json:"comment" form:"comment"`
+	Accuracy float32    `bson:"accuracy" json:"accuracy" form:"accuracy"`
 	Media    MediaInput `bson:"media" json:"media" form:"media"`
 }
 
@@ -22,16 +26,20 @@ type PostOutput struct {
 	Bird      BirdOutput  `bson:"bird" json:"bird" form:"bird"`
 	CreatedAt string      `bson:"created_at" json:"createdAt" form:"createdAt"`
 	Location  string      `bson:"location" json:"location" form:"location"`
+	Comment   string      `bson:"comment" json:"comment" form:"comment"`
+	Accuracy  float32     `bson:"accuracy" json:"accuracy" form:"accuracy"`
 	UserMedia MediaOutput `bson:"user_media" json:"userMedia" form:"userMedia"`
 }
 
 func PostDBToOutput(db *PostDB, user *UserOutput, bird *BirdOutput, media *MediaOutput) *PostOutput {
 	return &PostOutput{
-		Id:     	db.Id,
-		User:   	*user,
-		Bird:      	*bird,
-		CreatedAt: 	db.CreatedAt,
-		Location:  	db.Location, 
-		UserMedia: 	*media,
+		Id:        db.Id,
+		User:      *user,
+		Bird:      *bird,
+		CreatedAt: db.CreatedAt,
+		Location:  db.Location,
+		Comment:   db.Comment,
+		Accuracy:  db.Accuracy,
+		UserMedia: *media,
 	}
 }
