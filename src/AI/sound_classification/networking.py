5d74c8598daf300e88b02ca1eeb70d2307ee1f9b
Anton Lindén <linano-9@student.ltu.se>
2023-11-13 17:31:25 +0100

    Added the sound module and added a endpoint to evaluate a spectrogram from the AI controller.

--- src/AI/sound_classification/networking.py
+++ src/AI/sound_classification/networking.py
@@ -1,5 +1,9 @@
-from flask import Flask
+import numpy as np
+from PIL import Image
+from flask import Flask, jsonify, request
+from keras.preprocessing.image import ImageDataGenerator
 
+import classification
 import utils
 
 app = Flask(__name__)
@@ -12,7 +16,58 @@ def listen():
 
 @app.route('/process_sound', methods=['POST'])
 def process_sound():
-    pass
+    data = request.get_json()
+
+    _prefix_path = "images/"
+    _folder_name = utils.generate_random_name()
+
+    utils.create_folder(_prefix_path + _folder_name)
+
+    _full_path = _prefix_path + _folder_name + "/bird01"
+    utils.create_folder(_prefix_path + _folder_name + "/bird01")
+
+    width = data["width"]
+    height = data["height"]
+    mode = data["mode"]
+    pixels = [value for pixel in data["pixels"] for value in pixel]
+
+    _pixel_bytes = bytes(pixels)    # Create a byte array from the pixel data
+
+    _image_from_bytes = Image.frombytes(mode, (width, height), _pixel_bytes)   # Create a PIL image from the pixel data
+
+    _image_from_bytes.save(_full_path + "/image.png")   # Future, fix so that more birds can be saved.
+    _image_from_bytes.show()
+    general_datagen = ImageDataGenerator(rescale=1. / 255)  # for training, validation and testing data
+
+    testr_directory = _prefix_path + _folder_name
+
+    testr_generator = general_datagen.flow_from_directory(
+        testr_directory,
+        target_size=(224, 224),
+        batch_size=1
+    )
+
+    _result = classification.predict(classification.load_model(), testr_generator[0][0])
+
+    # Get the predicted class probabilities for all classes
+    predicted_probabilities = _result[0]
+
+    # Get the predicted class probability
+    predicted_class_probability = np.argsort(predicted_probabilities)[::-1]
+
+    # Select the top three classes with the highest probabilities
+    top_three_indices = predicted_class_probability[:3]
+
+    utils.delete_folder(_prefix_path + _folder_name)
+
+    _pos = int(np.argmax(_result, axis=-1)[0])
+
+    _label = get_label_from_position(_pos)
+
+    # Get the corresponding probabilities
+    top_three_probabilities = [predicted_probabilities[i] for i in top_three_indices]
+
+    return jsonify({'message': 'Image successfully received and processed', 'label': _label, 'accuracy': float(top_three_probabilities[0])})
 
 
 def get_label_from_position(_position):
